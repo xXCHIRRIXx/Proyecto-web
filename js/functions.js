4a69a28b38e3cdb14d918940da63f6cb6a6df68c
xXCHIRRIXx <166642189+xXCHIRRIXx@users.noreply.github.com>
2024-10-12 13:09:03 -0500

    actualizacion

--- js/functions.js
+++ js/functions.js
@@ -69,4 +69,44 @@ const satisfactionChart = new Chart(ctxSatisfaction, {
     }
 });
 
+// Simula el inicio de sesión y almacenamiento de los valores contables
+function iniciarSesion() {
+    localStorage.setItem('loggedIn', 'true');
+    localStorage.setItem('valorActivos', '500000');
+    localStorage.setItem('valorPasivos', '200000');
+    localStorage.setItem('valorPatrimonio', '300000');
+    localStorage.setItem('valorIngresos', '45000');
+    localStorage.setItem('valorGastos', '12500');
+    localStorage.setItem('valorCostosOperacion', '5000');
+}
+
+// Verifica si el usuario ha iniciado sesión y actualiza los valores
+window.onload = function() {
+    if (localStorage.getItem('loggedIn') === 'true') {
+        document.getElementById('valor-activos').textContent = `$${localStorage.getItem('valorActivos')}`;
+        document.getElementById('valor-pasivos').textContent = `$${localStorage.getItem('valorPasivos')}`;
+        document.getElementById('valor-patrimonio').textContent = `$${localStorage.getItem('valorPatrimonio')}`;
+        document.getElementById('valor-ingresos').textContent = `$${localStorage.getItem('valorIngresos')}`;
+        document.getElementById('valor-gastos').textContent = `$${localStorage.getItem('valorGastos')}`;
+        document.getElementById('valor-costos-operacion').textContent = `$${localStorage.getItem('valorCostosOperacion')}`;
+    }
+}
+
+        // Función para mostrar la sección correspondiente
+        function showSection(sectionId) {
+            // Oculta todas las secciones
+            const sections = document.querySelectorAll('.content-section');
+            sections.forEach(section => {
+                section.style.display = 'none';
+            });
+
+            // Muestra la sección seleccionada
+            const selectedSection = document.getElementById(sectionId);
+            selectedSection.style.display = 'block';
+        }
+
+        // Muestra la sección de perfil por defecto al cargar la página
+        document.addEventListener('DOMContentLoaded', () => {
+            showSection('perfil');
+        });
 
